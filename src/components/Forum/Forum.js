b145e3b770adea38280e74b575d58225d5160f06
kavantan <kavantan10@gmail.com>
2022-06-22 17:24:34 +0800

    Edited Forum.js to display foodstore database fields

--- src/components/Forum/Forum.js
+++ src/components/Forum/Forum.js
@@ -1,10 +1,37 @@
-import styles from "./Forum.module.css";
-import placeholder from "../../assets/forum-placeholder.png";
+import { useState, useEffect } from "react";
+import { getDocs } from "firebase/firestore";
+import { foodstoreCollectionRef } from "../../config/firebase.collections";
 
 const Forum = () => {
+  const [foodstores, setFoodstores] = useState([]);
+
+  useEffect(() => {
+    getFoodstores();
+  }, []); // Runs on first render, might change later
+
+  const getFoodstores = () => {
+    getDocs(foodstoreCollectionRef)
+      .then((response) => {
+        const fs = response.docs.map((doc) => ({
+          data: doc.data(),
+          id: doc.id,
+        }));
+        setFoodstores(fs);
+      })
+      .catch((error) => console.log(error.message));
+  };
+
   return (
     <div>
-      <img src={placeholder} alt="Forum" className={styles.Center}></img>
+      <u1>
+        {foodstores.map((foodstore) => (
+          <>
+            <h2> {foodstore.data.title}</h2>
+            <li> {foodstore.data.desc} </li>
+            <li>number of upvotes: {foodstore.data.upvotes}</li>
+          </>
+        ))}
+      </u1>
     </div>
   );
 };
