ebba277ec9c6211871ca8e20b7a19164247e2097
kavantan <kavantan10@gmail.com>
2022-06-24 19:53:04 +0800

    Added simple button to navigate to createpost

--- src/components/Forum/Forum.js
+++ src/components/Forum/Forum.js
@@ -5,6 +5,8 @@ import { useAuth } from "../../hooks/useAuth.js";
 import { db } from "../../config/firebaseConfig";
 import { postsCollectionRef } from "../../config/firebase.collections";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { Tooltip } from "@mui/material";
+import { NavLink } from "react-router-dom";
 
 function Forum() {
   const { user } = useAuth();
@@ -31,35 +33,47 @@ function Forum() {
   };
 
   return (
-    <div className={styles.homePage}>
-      {posts.map((post) => {
-        return (
-          <div className={styles.post}>
-            <div className={styles.postHeader}>
-              <div className={styles.title}>
-                <h1> {post.data.title}</h1>
+    <>
+      <button size="large" aria-label="create new post" color="inherit">
+        <NavLink
+          to="/createpost"
+          style={{ textDecoration: "inherit", color: "inherit" }}
+        >
+          Post a new Review
+        </NavLink>
+      </button>
+      <div className={styles.homePage}>
+        {posts.map((post) => {
+          return (
+            <div className={styles.post}>
+              <div className={styles.postHeader}>
+                <div className={styles.title}>
+                  <h1> {post.data.title}</h1>
+                </div>
+                <div className={styles.deletePost}>
+                  {user && post.data.author.id === user.uid && (
+                    <Tooltip title="Delete">
+                      <button
+                        onClick={() => {
+                          deletePost(post.id);
+                        }}
+                      >
+                        <DeleteIcon />
+                      </button>
+                    </Tooltip>
+                  )}
+                </div>
               </div>
-              <div className={styles.deletePost}>
-                {user && post.data.author.id === user.uid && (
-                  <button
-                    onClick={() => {
-                      deletePost(post.id);
-                    }}
-                  >
-                    <DeleteIcon />
-                  </button>
-                )}
+              <div className={styles.postTextContainer}>
+                {" "}
+                {post.data.postText}{" "}
               </div>
+              <h3>Posted by User: {post.data.author.name}</h3>
             </div>
-            <div className={styles.postTextContainer}>
-              {" "}
-              {post.data.postText}{" "}
-            </div>
-            <h3>Posted by User: {post.data.author.name}</h3>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
