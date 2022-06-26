87ab7fa24c775d0025d9de0dddbc160b4bf7d2e6
kavantan <kavantan10@gmail.com>
2022-06-26 23:09:19 +0800

    Display actual store details on StoreDetails.js

--- src/components/StoreDetails/StoreDetails.js
+++ src/components/StoreDetails/StoreDetails.js
@@ -1,5 +1,41 @@
+import styles from "./StoreDetails.module.css";
+import { useEffect, useState } from "react";
+import { getDocs } from "firebase/firestore";
+import { foodstoreCollectionRef } from "../../config/firebase.collections";
+
 const StoreDetails = ({ storeDir }) => {
-  return <div>{storeDir}</div>;
+  const [foodstores, setFoodstores] = useState([]);
+
+  useEffect(() => {
+    getFoodstores();
+  }, []);
+
+  const getFoodstores = () => {
+    getDocs(foodstoreCollectionRef)
+      .then((response) => {
+        const fs = response.docs.map((doc) => ({
+          data: doc.data(),
+          id: doc.id,
+        }));
+        setFoodstores(fs);
+      })
+      .catch((error) => console.log(error.message));
+  };
+
+  return (
+    <div className={styles.storeDetailsPage}>
+      {foodstores.map((foodstore) => {
+        return (
+          foodstore.data.dir === storeDir && (
+            <>
+              <div className={styles.storeTitle}>{foodstore.data.title}</div>
+              <div className={styles.storeDesc}>{foodstore.data.desc}</div>
+            </>
+          )
+        );
+      })}
+    </div>
+  );
 };
 
 export default StoreDetails;
