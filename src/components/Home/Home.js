594b33c9de83ad22688760ef7226446974defd96
kavantan <kavantan10@gmail.com>
2022-06-26 17:17:55 +0800

    Added featured store of the day to homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,30 @@
 import styles from "./Home.module.css";
 import Button from "@mui/material/Button";
+import { useEffect, useState } from "react";
+import { getDocs, limit, query } from "firebase/firestore";
+import { foodstoreCollectionRef } from "../../config/firebase.collections";
 import { NavLink } from "react-router-dom";
 
 const Home = () => {
+  const [foodstores, setFoodstores] = useState([]);
+
+  useEffect(() => {
+    getFoodstores();
+  }, []);
+
+  const getFoodstores = () => {
+    const q = query(foodstoreCollectionRef, limit(1));
+    getDocs(q)
+      .then((response) => {
+        const fs = response.docs.map((doc) => ({
+          data: doc.data(),
+          id: doc.id,
+        }));
+        setFoodstores(fs);
+      })
+      .catch((error) => console.log(error.message));
+  };
+
   return (
     <div className={styles.homePage}>
       <div className={styles.button}>
@@ -12,6 +34,20 @@ const Home = () => {
           </NavLink>
         </Button>
       </div>
+
+      {foodstores.map((foodstore) => {
+        return (
+          <div className={styles.post}>
+            <div className={styles.postHeader}>
+              <div className={styles.title}>{foodstore.data.title}</div>
+            </div>
+            <div className={styles.postTextContainer}>
+              {" "}
+              {foodstore.data.desc}{" "}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
