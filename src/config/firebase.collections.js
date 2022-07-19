ed433d841fc6d7bdeeef55aa8bec78e60030d019
kavantan <kavantan10@gmail.com>
2022-07-19 12:12:57 +0800

    Added usersCollectionRef into collections config

--- src/config/firebase.collections.js
+++ src/config/firebase.collections.js
@@ -4,3 +4,4 @@ import { db } from "./firebaseConfig";
 export const foodstoreCollectionRef = collection(db, "foodstores");
 export const postsCollectionRef = collection(db, "posts");
 export const dealsCollectionRef = collection(db, "deals");
+export const usersCollectionRef = collection(db, "users");
