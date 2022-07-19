8ad20674ee55e9d399d1b9db9ff47a8f36a58235
kavantan <kavantan10@gmail.com>
2022-07-19 22:10:23 +0800

    Added checklist into randomiser

--- src/components/Randomiser/Randomiser.js
+++ src/components/Randomiser/Randomiser.js
@@ -2,18 +2,46 @@ import CloseIcon from "@mui/icons-material/Close";
 import styles from "./Randomiser.module.css";
 import { useEffect, useState } from "react";
 import { getDocs } from "firebase/firestore";
-import { foodstoreCollectionRef } from "../../config/firebase.collections";
+import {
+  foodstoreCollectionRef,
+  locationsCollectionRef,
+} from "../../config/firebase.collections";
 import { useNavigate } from "react-router-dom";
 
 const Randomiser = ({ open, onClose }) => {
   const [foodstores, setFoodstores] = useState([]);
+  const [locations, setLocations] = useState([]);
+  const [checked, setChecked] = useState([]);
   let navigate = useNavigate();
 
   useEffect(() => {
     if (open) {
       getFoodstores();
+      getLocations();
     }
   }, [open]);
+
+  const handleCheck = (event) => {
+    var updatedList = [...checked];
+    if (event.target.checked) {
+      updatedList = [...checked, event.target.value];
+    } else {
+      updatedList.splice(checked.indexOf(event.target.value), 1);
+    }
+    setChecked(updatedList);
+  };
+
+  const getLocations = () => {
+    getDocs(locationsCollectionRef)
+      .then((response) => {
+        const fs = response.docs.map((doc) => ({
+          data: doc.data(),
+          id: doc.id,
+        }));
+        setLocations(fs);
+      })
+      .catch((error) => console.log(error.message));
+  };
 
   const getFoodstores = () => {
     getDocs(foodstoreCollectionRef)
@@ -42,7 +70,19 @@ const Randomiser = ({ open, onClose }) => {
                 <CloseIcon />
               </p>
               <div className={styles.content}>
-                Can't choose what to eat? Look no further!
+                Randomiser Filter (Click to Include in Search):
+                <div className={styles.checkboxes}>
+                  {locations.map((location) => (
+                    <div>
+                      <input
+                        value={location.id}
+                        type="checkbox"
+                        onChange={handleCheck}
+                      />
+                      <span>{location.id}</span>
+                    </div>
+                  ))}
+                </div>
               </div>
               <div className={styles.btnContainer}>
                 <button
