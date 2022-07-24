639fda480f33ffe916897e67aa2cae38c2d6d734
kavantan <kavantan10@gmail.com>
2022-07-24 15:43:34 +0800

    Encapsulation for firebase database in UserDetails

--- src/components/UserDetails/UserDetails.js
+++ src/components/UserDetails/UserDetails.js
@@ -7,7 +7,6 @@ import {
   postsCollectionRef,
   foodstoreCollectionRef,
 } from "../../config/firebase.collections";
-import { db } from "../../config/firebaseConfig";
 import { useNavigate } from "react-router-dom";
 import { Tooltip } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
@@ -50,7 +49,7 @@ const UserDetails = ({ username }) => {
 
   const deletePost = (id) => {
     setRandstate(randstate + 1);
-    deleteDoc(doc(db, "posts", id));
+    deleteDoc(doc(postsCollectionRef, id));
   };
 
   const getFoodstores = () => {
